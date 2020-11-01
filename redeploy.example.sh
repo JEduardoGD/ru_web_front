787b165e28a01ca167e26c3b7d7a021980f57bbc
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2020-11-01 17:27:36 -0600

    final fixes dockerize

--- redeploy.example.sh
+++ redeploy.example.sh
@@ -4,3 +4,5 @@ git pull origin branch
 docker-compose build
 docker-compose up -d
 docker-compose exec app mkdir -p /var/www/storage/framework/{sessions,views,cache}
+docker-compose exec app composer install
+docker-compose exec app php artisan key:generate
